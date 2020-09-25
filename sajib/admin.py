281525926cb52b4f88b5e747cc62110790c76896
manikdae34 <manikdae34@gmail.com>
2020-09-25 17:48:04 +0600

    first commit

---
+++ sajib/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import Person, Division, District, Upazila
+
+admin.site.register(Person)
+admin.site.register(Division)
+admin.site.register(District)
+admin.site.register(Upazila)
